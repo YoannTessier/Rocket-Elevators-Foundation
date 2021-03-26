692f2dcbfe8cd13101d2c39a52ad423f7055779d
YoannTessuer <yoann.tessier@hotmail.com>
2021-03-26 10:18:57 -0400

    Updated info for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,8 +3,8 @@ lock "~> 3.15.0"
 
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
-set :application, "SirineAyadi"
-set :repo_url, "https://github.com/ayadisirine/Rocket_Elevators_API.git"
+set :application, "YoannTessier"
+set :repo_url, "https://github.com/YoannTessier/Rocket-Elevators-Foundation.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
