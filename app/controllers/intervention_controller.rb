e47b5d160cb351ea4663583fc49d66f78a99eddc
YoannTessuer <yoann.tessier@hotmail.com>
2021-03-26 21:40:50 -0400

    Fixed controller so it send only intervention type to DB

--- app/controllers/intervention_controller.rb
+++ app/controllers/intervention_controller.rb
@@ -1,5 +1,7 @@
 class InterventionController < ApplicationController
+  require 'zendesk_api'
 
+  before_action :authenticate_user!
   # GETS COMPANY NAME AND ID + APPENDS IT IN @CUSTOMERS ARRAY
   def getCustomers
     @customers = []
@@ -50,57 +52,67 @@ class InterventionController < ApplicationController
     render json: {elevators: @elevatorList}
   end
 
-  # CREATES A NEW INSTANCE OF INTERVENTION AND PUTS IT IN TABLE
-  def create_intervention
-        
-    interventions = Intervention.new
-    interventions.author = current_user.id
-    interventions.customer_id = params[:customer_id]
-    interventions.building_id = params[:building_id]
-    interventions.battery_id = params[:battery_id]
-    interventions.column_id = params[:column_id]
-    interventions.elevator_id = params[:elevator_id]
-    interventions.employee_id = params[:employee_id]
-    interventions.report = params[:description]
-
-    interventions.save!
-
-      if interventions.save
-        create_intervention_ticket()
-        redirect_to '/interventions'
-      end
+  def new 
+    @intervention = Intervention.new
   end
 
-  # CREATES A NEW ZENDESK TICKET
-  def create_intervention_ticket()
-    client = ZendeskAPI::Client.new do |config|
-        config.url = ENV['ZENDESK_URL']
-        config.username = ENV['ZENDESK_USERNAME']
-        config.token = ENV['ZENDESK_TOKEN']
+  def create
+    @intervention = Intervention.new(intervention_params)
+    @intervention.author = current_user.id
+
+    @buildingId = @intervention.building_id
+    @batteryId = @intervention.battery_id
+    @columnId = @intervention.column_id
+    @elevatorId = @intervention.elevator_id
+    @author = @intervention.author
+
+    if (@intervention.elevator_id != nil && @intervention.elevator_id != " ")
+      @intervention.building_id, @intervention.battery_id, @intervention.column_id = nil
+    elsif (@intervention.column_id != nil && @intervention.column_id != " ")
+      @intervention.building_id, @intervention.battery_id, @intervention.elevator_id = nil
+    elsif (@intervention.battery_id != nil && @intervention.battery_id != " ")
+      @intervention.building_id, @intervention.column_id, @intervention.elevator_id = nil
+    elsif (@intervention.building_id != nil && @intervention.building_id != " ")
+      @intervention.battery_id, @intervention.column_id, @intervention.elevator_id = nil
     end
-    ZendeskAPI::Ticket.create!(client, 
+
+    if @intervention.save!
+      client = ZendeskAPI::Client.new do |client|
+        client.url = 'https://rocketelevators5995.zendesk.com/api/v2'
+        client.username = ENV['ZENDESK_USERNAME']
+        client.token = ENV['ZENDESK_TOKEN']
+      end
+
+      ZendeskAPI::Ticket.create!(client, 
         :subject => "Intervention",
         :comment => { 
           :value => " 
           Intervention Request: \n
           - Requester: #{Employee.find_by(user_id: current_user.id).first_name} #{Employee.find_by(user_id: current_user.id).last_name}
-          - Customer: #{Customer.find(params[:customer_id]).company_name}
-          - Building ID: #{params[:building_id]} 
-          - Battery ID: #{params[:battery_id]}
-          - Column ID:  #{params[:column_id]} 
-          - Elevator ID: #{params[:elevator_id]}
-          - Assigned Employee: #{Employee.find(params[:employee_id]).first_name} #{Employee.find(params[:employee_id]).last_name}\n
+          - Customer: #{Customer.find(@intervention.customer_id).company_name}
+          - Building ID: #{@buildingId} 
+          - Battery ID: #{@batteryId}
+          - Column ID:  #{@columnId} 
+          - Elevator ID: #{@elevatorId}
+          - Assigned Employee: #{Employee.find(@intervention.employee_id).first_name} #{Employee.find(@intervention.employee_id).last_name}\n
 
-          Description: #{params[:description]}"
+          Description: #{@intervention.report}"
           }, 
-        :requester => client.current_user.id,
+        :requester => current_user.id,
         :priority => "normal",
-        :type => "problem"
-      )
+        :type => "problem")
+      end
+        redirect_to '/interventions'
   end
 
   helper_method :getCustomers
   helper_method :getEmployees
+
+  private
+
+  def intervention_params
+    params.permit(:author, :intervention_start, :intervention_end, :result, :status, :customer_id, :building_id, :battery_id, :column_id, :elevator_id, :report, :employee_id)
+  end
 end
 
 
